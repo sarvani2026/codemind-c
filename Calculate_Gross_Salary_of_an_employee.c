fe40fe5481fab6fd38dedd427c1f2ca273e2d174
sarvani2026 <119731735+sarvani2026@users.noreply.github.com>
2022-12-20 20:04:31 +0530

    #include<stdio.h> int main() {     float bs,da,hra;     scanf("%f%f%f",&bs,&hra,&da);     float pf,gs;     pf=0.12*bs;     gs=bs+hra+da+pf;     printf("%0.2f %0.2f",pf,gs); }

---
+++ Calculate_Gross_Salary_of_an_employee.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    float bs,da,hra;
+    scanf("%f%f%f",&bs,&hra,&da);
+    float pf,gs;
+    pf=0.12*bs;
+    gs=bs+hra+da+pf;
+    printf("%0.2f
+%0.2f",pf,gs);
+}
