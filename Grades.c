277e54043e5fef60782423676abce330e06e90f9
sarvani2026 <119731735+sarvani2026@users.noreply.github.com>
2022-12-21 20:15:35 +0530

    #include<stdio.h> #include<math.h> int main() {     int a,b,c,d,e,per;     scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);     per=(a+b+c+d+e)/5;     if(per>=90)     {         printf("Grade A");     }     else if(per>=80)     {         printf("Grade B");     }     else if(per>=70)     {         printf("Grade C");     }     else if(per>=60)     {         printf("Grade D");     }     else if(per>=40)     {         printf("Grade E");     }     else if(per<70)     {         printf("Grade F");     }
        }

---
+++ Grades.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a,b,c,d,e,per;
+    scanf("%d%d%d%d%d",&a,&b,&c,&d,&e);
+    per=(a+b+c+d+e)/5;
+    if(per>=90)
+    {
+        printf("Grade A");
+    }
+    else if(per>=80)
+    {
+        printf("Grade B");
+    }
+    else if(per>=70)
+    {
+        printf("Grade C");
+    }
+    else if(per>=60)
+    {
+        printf("Grade D");
+    }
+    else if(per>=40)
+    {
+        printf("Grade E");
+    }
+    else if(per<70)
+    {
+        printf("Grade F");
+    }
+    
+    }
+
