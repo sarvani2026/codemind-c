46eaf9b7428612b2ccb0f5b248dc55dde52e8880
sarvani2026 <119731735+sarvani2026@users.noreply.github.com>
2022-12-22 09:15:22 +0530

    #include<stdio.h> int main() {     int x,y,z,p;     scanf("%d%d%d",&x,&y,&z);     p=(y*1)+(z*2);     if(p>=x)     {     printf("Qualify");     }     else     {      printf("Not Qualify");     } }

---
+++ Contest:.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int x,y,z,p;
+    scanf("%d%d%d",&x,&y,&z);
+    p=(y*1)+(z*2);
+    if(p>=x)
+    {
+    printf("Qualify");
+    }
+    else
+    {
+     printf("Not Qualify");  
+    }
+}
