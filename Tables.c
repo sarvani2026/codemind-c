0e1d805bebe0b1bcb3fe76d638637ad3c9a75e2f
sarvani2026 <119731735+sarvani2026@users.noreply.github.com>
2023-06-02 11:28:24 +0530

    #include<stdio.h> int main() {     int i,n,r;     scanf("%d%d",&n,&r);     for(i=1;i<=r;++i)     {         if(i%2==0)         {             continue;         }         printf("%d x %d = %d ",n,i,n*i);     }     return 0; }

--- Tables.c
+++ Tables.c
@@ -1,20 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,r;
+    int i,n,r;
     scanf("%d%d",&n,&r);
     for(i=1;i<=r;++i)
     {
         if(i%2==0)
         {
             continue;
-            
         }
-        else
-        {
-            printf("%d x %d = %d
+        printf("%d x %d = %d
 ",n,i,n*i);
-        }
     }
     return 0;
 }
